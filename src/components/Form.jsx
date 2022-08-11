7e7d7c809d38af7e4e3fb1fa2ecb37cf36271e77
katmart93 <katwlodarczyk93@gmail.com>
2022-08-11 14:46:37 +0200

    firm component updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 
-export default function Form() {
+export default function Form({
+  title,
+  setTitle,
+  author,
+  setAuthor,
+  isbn,
+  setIsbn,
+  currBookId,
+}) {
   return (
     <form>
       <label htmlFor="title">Title</label>
-      <input type="text" id="title" name="title" />
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.set.target.value)}
+      />
       <label htmlFor="author">Author</label>
-      <input type="text" id="author" name="author" />
+      <input
+        type="text"
+        id="author"
+        name="author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
       <label htmlFor="isbn">ISBN#</label>
-      <input type="text" id="isbn" name="isbn" />
+      <input
+        type="text"
+        id="isbn"
+        required
+        name="isbn"
+        value={isbn}
+        onChange={(e) => setIsbn(e.target.value)}
+      />
+      <button tabIndex="0" type="submit">
+        {currBookId !== null ? "Update" : "Add"}
+      </button>
     </form>
   );
 }
