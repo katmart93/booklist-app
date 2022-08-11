f259c67b924b22e5f2fc71e09a78cd6407762e7c
katmart93 <katwlodarczyk93@gmail.com>
2022-08-11 12:58:09 +0200

    states created in app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 // styles
 import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isbn, setIsbn] = useState("");
+  const [currBookId, setCurrBookId] = useState(null);
+  const [books, setBooks] = useState([
+    {
+      bookTitle: title,
+      bookAuthor: author,
+      bookIsbn: isbn,
+      id: uuidv4(),
+    },
+  ]);
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <div className="container"></div>
     </div>
   );
 }
