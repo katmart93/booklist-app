ae5054e3bd8999e739c79bac578d5916c7a8545e
katmart93 <katwlodarczyk93@gmail.com>
2022-08-11 13:02:17 +0200

    inputs created in the form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <form>
+      <label htmlFor="title">Title</label>
+      <input type="text" id="title" name="title" />
+      <label htmlFor="author">Author</label>
+      <input type="text" id="author" name="author" />
+      <label htmlFor="isbn">ISBN#</label>
+      <input type="text" id="isbn" name="isbn" />
+    </form>
+  );
+}
+author;
